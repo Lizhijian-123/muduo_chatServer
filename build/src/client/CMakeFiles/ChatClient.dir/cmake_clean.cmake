7f28e55113aa920ddea3cb89abc5f2d7b72a387a
root <2229881459@qq.com>
2023-08-02 19:51:08 +0800

    muduo_chatserver 尝试git 代码不是自己写的,也没有编译过

---
+++ build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/root/CPlusPlusProject/muduo集群聊天/chatserver-master/bin/ChatClient"
+  "/root/CPlusPlusProject/muduo集群聊天/chatserver-master/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
